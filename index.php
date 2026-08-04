19547ab72d6437514af14977ab766e1180c16c0f
Nitiema Allassane <allassanenitiema@gmail.com>
2026-08-04 21:27:18 +0000

    Install tailwind css CLI

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<?php
-require_once('./configs/database.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/output.css" rel="stylesheet">
+    <title>Ma Tontine</title>
+</head>
+<body>
 
-$message = "";
-if ($pdo_connexion) {
-    $message = "Connexion reussi !";
-} else {
-    $message = "Connexion echoué !";
-}
-
-?>
-
-<h1>
-    <?php echo $message ?>
-</h1>
+   
+    
+</body>
+</html>
